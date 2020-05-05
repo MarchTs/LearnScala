5f8a55c470361755dccbabe16a1fbf146e191778
March <m.hardwork@outlook.com>
2020-05-05 17:27:35 +0700

    try using reduce append string and try foldleft to solve compress problem

--- src/exercies/the99/P08.scala
+++ src/exercies/the99/P08.scala
@@ -5,9 +5,17 @@ object P08 {
   def main(args: Array[String]): Unit = {
     val unCompress = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
 
-    val someResult = compress(unCompress)
-    println(someResult)
+    val recursiveResult = compress(unCompress)
+    println(s"Compress with recursive ${recursiveResult}")
 
+    val compressUsingReduceResult = compressUsingReduce(unCompress)
+    println(s"Compress with Reduce ${compressUsingReduceResult}")
+
+    val compressUsingfoldLeftResult = compressUsingfoldLeft(unCompress)
+    println(s"Compress with foldLeft ${compressUsingfoldLeftResult}")
+
+    val compressUsingfoldLeft2Result = compressUsingfoldLeft2(unCompress)
+    println(s"Compress with foldLeft2 ${compressUsingfoldLeft2Result}")
   }
 
   // without peek solution
@@ -20,6 +28,29 @@ object P08 {
         else _compress(tail, compressed)
       }
     }
+
     _compress(list)
+  }
+
+  def compressUsingReduce(list: List[String]): String = {
+    list.reduce((a, b) => {
+      if (a.isEmpty) b
+      else if (a.last.toString == b) a else a + b
+    })
+  }
+
+  def compressUsingfoldLeft(list: List[Any]): List[Any] = {
+    list.foldLeft(List[Any]()) { (a, b) =>
+      if (a.isEmpty) List(b)
+      else if (a.last == b) a
+      else a :+ b
+    }
+  }
+
+  def compressUsingfoldLeft2(list: List[Any]): List[Any] = {
+    list.foldLeft(List[Any]()) { (a, b) =>
+      if (a.isEmpty || a.last != b) a :+ b
+      else a
+    }
   }
 }
